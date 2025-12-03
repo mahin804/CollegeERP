349b37cc12766aec2315dbc99d408eb4f8c839d2
mahin804 <mahinmujeeb17@gmail.com>
2025-12-03 23:47:42 +0530

    login

--- src/Service.js/APIService.js
+++ src/Service.js/APIService.js
@@ -1,8 +1,8 @@
 import axios from "axios"
-const BaseURl = "https://localhost:44383"
+const BaseURl = "https://collegeerpapirepo-1.onrender.com"
 
 export const LoginPostApi = async (BodyData) => {
-    return await axios.post(`${BaseURl}/api/StudentRegister/login`, BodyData);
+    return await axios.post(`${BaseURl}/api/StudentRegister/login1`, BodyData);
 }
 export const GetDashboardCountsApi = async () => {
     return await axios.get(`${BaseURl}/api/StudentRegister/GetCounts`);
