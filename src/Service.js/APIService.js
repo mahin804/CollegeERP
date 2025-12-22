03f82644bfa2c24bf49b9da3a1bae785820654c4
mahin804 <mahinmujeeb17@gmail.com>
2025-12-23 00:10:59 +0530

    minor

--- src/Service.js/APIService.js
+++ src/Service.js/APIService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
- const BaseURl = "https://localhost:7288"
-//const BaseURl = "https://collegeerpapirepo-1.onrender.com"
+//const BaseURl = "https://localhost:7288"
+const BaseURl = "https://collegeerpapirepo-1.onrender.com"
 
 export const LoginPostApi = async (BodyData) => {
     return await axios.post(`${BaseURl}/api/StudentRegister/login1`, BodyData);
